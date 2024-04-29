b89c444ff2473b405f97c65ea46a9fd11e61f3c8
Saif Ali <37189361+saifali1035@users.noreply.github.com>
2024-04-29 04:08:17 +0000

    added second target group

--- Load_Balancer/Application_LB/ec2_instance.tf
+++ Load_Balancer/Application_LB/ec2_instance.tf
@@ -10,3 +10,16 @@ resource "aws_instance" "Web-server" {
   }
 }
 
+resource "aws_instance" "Web-server-2" {
+  count = 2
+  ami = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance-security-group.name]
+  user_data = file("${path.module}/second_script.sh")
+  associate_public_ip_address = "false"
+  tags = {
+    Name = "Web-Server-2"
+  }
+}
+
+
--- Load_Balancer/Application_LB/lb.tf
+++ Load_Balancer/Application_LB/lb.tf
@@ -1,3 +1,5 @@
+data "aws_elb_service_account" "elb_account_id" {}
+
 resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
@@ -60,10 +62,45 @@ resource "aws_lb_listener" "Web-Server-listener" {
   }
 }
 
-data "aws_elb_service_account" "elb_account_id" {}
 
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer."
-  value       = aws_lb.Web-server-lb.dns_name
+
+
+
+
+resource "aws_lb_target_group" "Web-server-2-TG" {
+  name     = "tf-web-server-2-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
 }
+
+resource "aws_lb_target_group_attachment" "Web-server-2-TG-attachment" {
+  for_each = {
+    for k, v in aws_instance.Web-server-2 :
+    k => v
+  }
+  target_group_arn = aws_lb_target_group.Web-server-2-TG.arn
+  target_id        = each.value.id
+  port             = 8000
+}
+
+resource "aws_lb_listener_rule" "Web-server-2-rule" {
+  listener_arn = aws_lb_listener.Web-Server-listener.arn
+ priority     = 60
+
+ action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.Web-server-2-TG.arn
+ }
+
+ condition {
+   path_pattern {
+     values = ["/second*"]
+   }
+ }
+}
+
+
+
+
 
